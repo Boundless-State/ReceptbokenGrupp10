8164ed0ca5dfc540b46021ea463f6550458b2acb
AkTobias <146948454+AkTobias@users.noreply.github.com>
2024-10-07 13:29:04 +0200

    Fixed Filehandler class

--- ReceptbokenGrupp10/ReceptbokenGrupp10/Form1.Designer.cs
+++ ReceptbokenGrupp10/ReceptbokenGrupp10/Form1.Designer.cs
@@ -44,97 +44,111 @@
             // comboBoxCategory
             // 
             comboBoxCategory.FormattingEnabled = true;
-            comboBoxCategory.Location = new Point(12, 31);
+            comboBoxCategory.Location = new Point(18, 46);
+            comboBoxCategory.Margin = new Padding(4, 4, 4, 4);
             comboBoxCategory.Name = "comboBoxCategory";
-            comboBoxCategory.Size = new Size(151, 28);
+            comboBoxCategory.Size = new Size(224, 38);
             comboBoxCategory.TabIndex = 2;
             // 
             // textBoxSearch
             // 
-            textBoxSearch.Location = new Point(12, 79);
+            textBoxSearch.Location = new Point(18, 118);
+            textBoxSearch.Margin = new Padding(4, 4, 4, 4);
             textBoxSearch.Name = "textBoxSearch";
-            textBoxSearch.Size = new Size(446, 27);
+            textBoxSearch.Size = new Size(667, 35);
             textBoxSearch.TabIndex = 3;
             // 
             // listBoxResult
             // 
             listBoxResult.FormattingEnabled = true;
-            listBoxResult.Location = new Point(12, 119);
+            listBoxResult.ItemHeight = 30;
+            listBoxResult.Location = new Point(18, 178);
+            listBoxResult.Margin = new Padding(4, 4, 4, 4);
             listBoxResult.Name = "listBoxResult";
-            listBoxResult.Size = new Size(546, 224);
+            listBoxResult.Size = new Size(817, 334);
             listBoxResult.TabIndex = 4;
             // 
             // buttonSearch
             // 
-            buttonSearch.Location = new Point(464, 79);
+            buttonSearch.Location = new Point(696, 118);
+            buttonSearch.Margin = new Padding(4, 4, 4, 4);
             buttonSearch.Name = "buttonSearch";
-            buttonSearch.Size = new Size(94, 29);
+            buttonSearch.Size = new Size(141, 44);
             buttonSearch.TabIndex = 5;
             buttonSearch.Text = "Sök";
             buttonSearch.UseVisualStyleBackColor = true;
             // 
             // textBoxRecipeTitle
             // 
-            textBoxRecipeTitle.Location = new Point(184, 31);
+            textBoxRecipeTitle.Location = new Point(276, 46);
+            textBoxRecipeTitle.Margin = new Padding(4, 4, 4, 4);
             textBoxRecipeTitle.Name = "textBoxRecipeTitle";
-            textBoxRecipeTitle.Size = new Size(374, 27);
+            textBoxRecipeTitle.Size = new Size(559, 35);
             textBoxRecipeTitle.TabIndex = 6;
             // 
             // buttonLogIn
             // 
-            buttonLogIn.Location = new Point(705, 119);
+            buttonLogIn.Location = new Point(1058, 178);
+            buttonLogIn.Margin = new Padding(4, 4, 4, 4);
             buttonLogIn.Name = "buttonLogIn";
-            buttonLogIn.Size = new Size(94, 29);
+            buttonLogIn.Size = new Size(141, 44);
             buttonLogIn.TabIndex = 7;
             buttonLogIn.Text = "Logga in";
             buttonLogIn.UseVisualStyleBackColor = true;
             // 
             // textBoxUser
             // 
-            textBoxUser.Location = new Point(674, 32);
+            textBoxUser.Location = new Point(1011, 48);
+            textBoxUser.Margin = new Padding(4, 4, 4, 4);
             textBoxUser.Name = "textBoxUser";
-            textBoxUser.Size = new Size(125, 27);
+            textBoxUser.Size = new Size(186, 35);
             textBoxUser.TabIndex = 8;
             // 
             // textBoxPassword
             // 
-            textBoxPassword.Location = new Point(674, 76);
+            textBoxPassword.Location = new Point(1011, 114);
+            textBoxPassword.Margin = new Padding(4, 4, 4, 4);
             textBoxPassword.Name = "textBoxPassword";
-            textBoxPassword.Size = new Size(125, 27);
+            textBoxPassword.Size = new Size(186, 35);
             textBoxPassword.TabIndex = 9;
             // 
             // labelUsername
             // 
             labelUsername.AutoSize = true;
-            labelUsername.Location = new Point(588, 35);
+            labelUsername.Location = new Point(882, 52);
+            labelUsername.Margin = new Padding(4, 0, 4, 0);
             labelUsername.Name = "labelUsername";
-            labelUsername.Size = new Size(83, 20);
+            labelUsername.Size = new Size(118, 30);
             labelUsername.TabIndex = 10;
             labelUsername.Text = "Användare:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(598, 79);
+            label2.Location = new Point(897, 118);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(73, 20);
+            label2.Size = new Size(103, 30);
             label2.TabIndex = 11;
             label2.Text = "Lösenord:";
             // 
             // textBoxIngredients
             // 
-            textBoxIngredients.Location = new Point(12, 349);
+            textBoxIngredients.Location = new Point(18, 524);
+            textBoxIngredients.Margin = new Padding(4, 4, 4, 4);
             textBoxIngredients.Multiline = true;
             textBoxIngredients.Name = "textBoxIngredients";
             textBoxIngredients.ReadOnly = true;
-            textBoxIngredients.Size = new Size(546, 264);
+            textBoxIngredients.Size = new Size(817, 394);
             textBoxIngredients.TabIndex = 12;
+            textBoxIngredients.Text = "7";
+            
             // 
             // FormRecipe
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(832, 633);
+            ClientSize = new Size(1248, 950);
             Controls.Add(textBoxIngredients);
             Controls.Add(label2);
             Controls.Add(labelUsername);
@@ -146,6 +160,7 @@
             Controls.Add(listBoxResult);
             Controls.Add(textBoxSearch);
             Controls.Add(comboBoxCategory);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "FormRecipe";
             Text = "Receptboken";
             ResumeLayout(false);
--- ReceptbokenGrupp10/ReceptbokenGrupp10/Form1.cs
+++ ReceptbokenGrupp10/ReceptbokenGrupp10/Form1.cs
@@ -1,10 +1,23 @@
+using ReceptbokenGrupp10.Classes;
+
 namespace ReceptbokenGrupp10
 {
     public partial class FormRecipe : Form
     {
+ 
+
         public FormRecipe()
         {
             InitializeComponent();
+           
         }
+
+
+        
+
+
+   
+
+        
     }
 }
