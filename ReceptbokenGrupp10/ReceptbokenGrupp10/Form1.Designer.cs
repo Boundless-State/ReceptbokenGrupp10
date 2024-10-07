d465c1fdaa1e0b01a053295a4f0c6e1b028d51ef
Boundless-State <timmy.windahl@gmail.com>
2024-10-07 10:48:42 +0200

    t

--- ReceptbokenGrupp10/ReceptbokenGrupp10/Form1.Designer.cs
+++ ReceptbokenGrupp10/ReceptbokenGrupp10/Form1.Designer.cs
@@ -28,12 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBoxIngredients = new ListBox();
+            listBoxRecipe = new ListBox();
+            comboBoxCategory = new ComboBox();
+            textBoxSearch = new TextBox();
+            listBoxResult = new ListBox();
+            buttonSearch = new Button();
+            textBoxRecipeTitle = new TextBox();
+            buttonLogIn = new Button();
+            textBoxUser = new TextBox();
+            textBoxPassword = new TextBox();
+            labelUsername = new Label();
+            label2 = new Label();
+            textBoxIngredients = new TextBox();
+            SuspendLayout();
+            // 
+            // listBoxIngredients
+            // 
+            listBoxIngredients.FormattingEnabled = true;
+            listBoxIngredients.Location = new Point(736, 330);
+            listBoxIngredients.Name = "listBoxIngredients";
+            listBoxIngredients.Size = new Size(268, 264);
+            listBoxIngredients.TabIndex = 0;
+            // 
+            // listBoxRecipe
+            // 
+            listBoxRecipe.FormattingEnabled = true;
+            listBoxRecipe.Location = new Point(12, 349);
+            listBoxRecipe.Name = "listBoxRecipe";
+            listBoxRecipe.Size = new Size(272, 264);
+            listBoxRecipe.TabIndex = 1;
+            // 
+            // comboBoxCategory
+            // 
+            comboBoxCategory.FormattingEnabled = true;
+            comboBoxCategory.Location = new Point(12, 31);
+            comboBoxCategory.Name = "comboBoxCategory";
+            comboBoxCategory.Size = new Size(151, 28);
+            comboBoxCategory.TabIndex = 2;
+            // 
+            // textBoxSearch
+            // 
+            textBoxSearch.Location = new Point(12, 79);
+            textBoxSearch.Name = "textBoxSearch";
+            textBoxSearch.Size = new Size(446, 27);
+            textBoxSearch.TabIndex = 3;
+            // 
+            // listBoxResult
+            // 
+            listBoxResult.FormattingEnabled = true;
+            listBoxResult.Location = new Point(12, 119);
+            listBoxResult.Name = "listBoxResult";
+            listBoxResult.Size = new Size(546, 224);
+            listBoxResult.TabIndex = 4;
+            // 
+            // buttonSearch
+            // 
+            buttonSearch.Location = new Point(464, 79);
+            buttonSearch.Name = "buttonSearch";
+            buttonSearch.Size = new Size(94, 29);
+            buttonSearch.TabIndex = 5;
+            buttonSearch.Text = "Sök";
+            buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // textBoxRecipeTitle
+            // 
+            textBoxRecipeTitle.Location = new Point(184, 31);
+            textBoxRecipeTitle.Name = "textBoxRecipeTitle";
+            textBoxRecipeTitle.Size = new Size(374, 27);
+            textBoxRecipeTitle.TabIndex = 6;
+            // 
+            // buttonLogIn
+            // 
+            buttonLogIn.Location = new Point(976, 122);
+            buttonLogIn.Name = "buttonLogIn";
+            buttonLogIn.Size = new Size(94, 29);
+            buttonLogIn.TabIndex = 7;
+            buttonLogIn.Text = "Logga in";
+            buttonLogIn.UseVisualStyleBackColor = true;
+            // 
+            // textBoxUser
+            // 
+            textBoxUser.Location = new Point(945, 35);
+            textBoxUser.Name = "textBoxUser";
+            textBoxUser.Size = new Size(125, 27);
+            textBoxUser.TabIndex = 8;
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Location = new Point(945, 79);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(125, 27);
+            textBoxPassword.TabIndex = 9;
+            // 
+            // labelUsername
+            // 
+            labelUsername.AutoSize = true;
+            labelUsername.Location = new Point(859, 38);
+            labelUsername.Name = "labelUsername";
+            labelUsername.Size = new Size(83, 20);
+            labelUsername.TabIndex = 10;
+            labelUsername.Text = "Användare:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(869, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 11;
+            label2.Text = "Lösenord:";
+            // 
+            // textBoxIngredients
+            // 
+            textBoxIngredients.Location = new Point(313, 349);
+            textBoxIngredients.Multiline = true;
+            textBoxIngredients.Name = "textBoxIngredients";
+            textBoxIngredients.ReadOnly = true;
+            textBoxIngredients.Size = new Size(245, 264);
+            textBoxIngredients.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1102, 633);
+            Controls.Add(textBoxIngredients);
+            Controls.Add(label2);
+            Controls.Add(labelUsername);
+            Controls.Add(textBoxPassword);
+            Controls.Add(textBoxUser);
+            Controls.Add(buttonLogIn);
+            Controls.Add(textBoxRecipeTitle);
+            Controls.Add(buttonSearch);
+            Controls.Add(listBoxResult);
+            Controls.Add(textBoxSearch);
+            Controls.Add(comboBoxCategory);
+            Controls.Add(listBoxRecipe);
+            Controls.Add(listBoxIngredients);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBoxIngredients;
+        private ListBox listBoxRecipe;
+        private ComboBox comboBoxCategory;
+        private TextBox textBoxSearch;
+        private ListBox listBoxResult;
+        private Button buttonSearch;
+        private TextBox textBoxRecipeTitle;
+        private Button buttonLogIn;
+        private TextBox textBoxUser;
+        private TextBox textBoxPassword;
+        private Label labelUsername;
+        private Label label2;
+        private TextBox textBoxIngredients;
     }
 }
