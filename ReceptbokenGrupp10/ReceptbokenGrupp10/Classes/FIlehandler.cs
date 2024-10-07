0ba525a81a70abac87514618aed9dba13708716e
AkTobias <146948454+AkTobias@users.noreply.github.com>
2024-10-07 11:15:21 +0200

    Filehandler added

---
+++ ReceptbokenGrupp10/ReceptbokenGrupp10/Classes/FIlehandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceptbokenGrupp10.Classes
+{
+    internal class FIlehandler
+    {
+    }
+}
