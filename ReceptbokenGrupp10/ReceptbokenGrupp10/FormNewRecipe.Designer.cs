7735874d2082140537a418e204ead367d0b59fc6
mapamapa1 <cw.carlwinter@gmail.com>
2024-10-10 15:00:15 +0200

    add-function-to-JSON

--- ReceptbokenGrupp10/ReceptbokenGrupp10/FormNewRecipe.Designer.cs
+++ ReceptbokenGrupp10/ReceptbokenGrupp10/FormNewRecipe.Designer.cs
@@ -40,68 +40,76 @@
             // labelTitle
             // 
             labelTitle.AutoSize = true;
-            labelTitle.Location = new Point(37, 26);
+            labelTitle.Location = new Point(46, 32);
+            labelTitle.Margin = new Padding(4, 0, 4, 0);
             labelTitle.Name = "labelTitle";
-            labelTitle.Size = new Size(38, 20);
+            labelTitle.Size = new Size(44, 25);
             labelTitle.TabIndex = 0;
             labelTitle.Text = "Titel";
             // 
             // textBoxTitle
             // 
-            textBoxTitle.Location = new Point(37, 49);
+            textBoxTitle.Location = new Point(46, 61);
+            textBoxTitle.Margin = new Padding(4, 4, 4, 4);
             textBoxTitle.Name = "textBoxTitle";
             textBoxTitle.PlaceholderText = "Titel";
-            textBoxTitle.Size = new Size(407, 27);
+            textBoxTitle.Size = new Size(508, 31);
             textBoxTitle.TabIndex = 1;
             // 
             // labelDescription
             // 
             labelDescription.AutoSize = true;
-            labelDescription.Location = new Point(37, 115);
+            labelDescription.Location = new Point(46, 144);
+            labelDescription.Margin = new Padding(4, 0, 4, 0);
             labelDescription.Name = "labelDescription";
-            labelDescription.Size = new Size(58, 20);
+            labelDescription.Size = new Size(69, 25);
             labelDescription.TabIndex = 2;
             labelDescription.Text = "Recept:";
             // 
             // labelCategory
             // 
             labelCategory.AutoSize = true;
-            labelCategory.Location = new Point(468, 25);
+            labelCategory.Location = new Point(585, 31);
+            labelCategory.Margin = new Padding(4, 0, 4, 0);
             labelCategory.Name = "labelCategory";
-            labelCategory.Size = new Size(66, 20);
+            labelCategory.Size = new Size(78, 25);
             labelCategory.TabIndex = 4;
             labelCategory.Text = "Kategori";
             // 
             // comboBoxCategory
             // 
             comboBoxCategory.FormattingEnabled = true;
-            comboBoxCategory.Location = new Point(468, 48);
+            comboBoxCategory.Location = new Point(585, 60);
+            comboBoxCategory.Margin = new Padding(4, 4, 4, 4);
             comboBoxCategory.Name = "comboBoxCategory";
-            comboBoxCategory.Size = new Size(151, 28);
+            comboBoxCategory.Size = new Size(188, 33);
             comboBoxCategory.TabIndex = 5;
             // 
             // buttonSave
             // 
-            buttonSave.Location = new Point(525, 500);
+            buttonSave.Location = new Point(656, 625);
+            buttonSave.Margin = new Padding(4, 4, 4, 4);
             buttonSave.Name = "buttonSave";
-            buttonSave.Size = new Size(94, 29);
+            buttonSave.Size = new Size(118, 36);
             buttonSave.TabIndex = 6;
             buttonSave.Text = "Spara";
             buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += buttonSave_Click;
             // 
             // textBoxRecipe
             // 
-            textBoxRecipe.Location = new Point(37, 138);
+            textBoxRecipe.Location = new Point(46, 172);
+            textBoxRecipe.Margin = new Padding(4, 4, 4, 4);
             textBoxRecipe.Name = "textBoxRecipe";
-            textBoxRecipe.Size = new Size(582, 356);
+            textBoxRecipe.Size = new Size(726, 444);
             textBoxRecipe.TabIndex = 7;
             textBoxRecipe.Text = "";
             // 
             // FormNewRecipe
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(646, 554);
+            ClientSize = new Size(808, 692);
             Controls.Add(textBoxRecipe);
             Controls.Add(buttonSave);
             Controls.Add(comboBoxCategory);
@@ -109,6 +117,7 @@
             Controls.Add(labelDescription);
             Controls.Add(textBoxTitle);
             Controls.Add(labelTitle);
+            Margin = new Padding(4, 4, 4, 4);
             Name = "FormNewRecipe";
             Text = "Nytt recept";
             ResumeLayout(false);
