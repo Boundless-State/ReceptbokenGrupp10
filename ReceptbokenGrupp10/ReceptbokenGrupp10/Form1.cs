31789776a44cf6967753216e4de694160de73f03
Marcus BV <marcus.backstromviol@gmail.com>
2024-10-15 10:29:54 +0200

    Update Form1.cs

--- ReceptbokenGrupp10/ReceptbokenGrupp10/Form1.cs
+++ ReceptbokenGrupp10/ReceptbokenGrupp10/Form1.cs
@@ -198,6 +198,7 @@ namespace ReceptbokenGrupp10
         {
             List <string> categories = new List<string>();
 
+            categories.Add("Alla");
 
             foreach (Recipe recipe in recipeList)
             {
